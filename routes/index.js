d0dfc877dc31c08276031f22cca3e8e1dfdf0a45
lumichael24 <gongminglu@gmail.com>
2019-11-22 02:15:23 -0500

    connect AWS mysql db to web app

---
+++ routes/index.js
@@ -0,0 +1,123 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+var config = require('../db-config.js');
+
+/* ----- Connects to your mySQL database ----- */
+
+var mysql = require('mysql');
+//var oracledb = require('oracledb')
+
+config.connectionLimit = 10;
+var connection = mysql.createPool(config);
+/*var connection = oracledb.getConnection({
+  connectString     : process.env.RDS_HOSTNAME,//'cis550.cjh0ekn9ikxr.us-east-1.rds.amazonaws.com:1521/BBALLDB',
+  //connectionLimit: 10,
+  user     : 'lu20',
+  password : 'password',
+  database : 'cis550',
+  port     : '1521'
+});*/
+
+
+/*
+connection.query('SELECT * FROM NBA WHERE ROWNUM <= 1', function (err, result, fields) {
+    if (err) throw new Error(err)
+})*/
+
+/* ------------------------------------------- */
+/* ----- Routers to handle FILE requests ----- */
+/* ------------------------------------------- */
+
+router.get('/', function(req, res) {
+  //console.log(connection)
+  res.sendFile(path.join(__dirname, '../', 'views', 'dashboard.html'));
+});
+
+/* ----- Q2 (Recommendations) ----- */
+router.get('/recommendations', function(req, res) {
+  res.sendFile(path.join(__dirname, '../', 'views', 'recommendations.html'));
+});
+
+/* ----- Q3 (Best Of Decades) ----- */
+router.get('/bestof', function(req, res) {
+  res.sendFile(path.join(__dirname, '../', 'views', 'bestof.html'));
+});
+
+/* ----- Bonus (Posters) ----- */
+router.get('/posters', function(req, res) {
+  res.sendFile(path.join(__dirname, '../', 'views', 'posters.html'));
+});
+
+router.get('/reference', function(req, res) {
+  res.sendFile(path.join(__dirname, '../', 'views', 'reference.html'));
+});
+
+/* Template for a FILE request router:
+
+Specifies that when the app recieves a GET request at <PATH>,
+it should respond by sending file <MY_FILE>
+
+router.get('<PATH>', function(req, res) {
+  res.sendFile(path.join(__dirname, '../', 'views', '<MY_FILE>'));
+});
+
+*/
+
+
+/* ------------------------------------------------ */
+/* ----- Routers to handle data requests ----- */
+/* ------------------------------------------------ */
+
+/* ----- Q1 (Dashboard) ----- */
+
+
+
+/* ----- Q2 (Recommendations) ----- */
+
+
+
+
+/* ----- Q3 (Best Of Decades) ----- */
+
+router.get('/decades', function(req, res) {
+  var query = `
+    SELECT * FROM Test
+  `;
+  console.log(connection);
+  connection.query(query, function(err, rows, fields) {
+    if (err) console.log(err);
+    else {
+      console.log("pu");
+      console.log(rows);
+      res.json(rows);
+    }
+  });
+
+});
+
+
+
+/* ----- Bonus (Posters) ----- */
+
+
+
+/* General Template for GET requests:
+
+router.get('/routeName/:customParameter', function(req, res) {
+  // Parses the customParameter from the path, and assigns it to variable myData
+  var myData = req.params.customParameter;
+  var query = '';
+  console.log(query);
+  connection.query(query, function(err, rows, fields) {
+    if (err) console.log(err);
+    else {
+      // Returns the result of the query (rows) in JSON as the response
+      res.json(rows);
+    }
+  });
+});
+*/
+
+
+module.exports = router;
